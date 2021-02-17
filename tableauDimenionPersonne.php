20efd81168c62487f162a3b0bda8d16b9f930c18
naziradidier <nazirap20.aps1b@gmai.com>
2021-02-17 15:22:26 +0100

    tableau multidimension pour personne

--- tableauDimenionPersonne.php
+++ tableauDimenionPersonne.php
@@ -1,17 +1,45 @@
 
 <?php
-    $cars = array (
-      array("Nom","Nazirah"),
-      array("Prénom","Prisca"),
-      array("age",19),
-      array("Lieu","Ambatoroka")
+    $Personne = array (
+        $colonne = array("Nom", "Nazirah", "Miora", "Toavina"),
+        $colonne = array("Prénom","Prisca", "Jelisca", "Mbola"),
+        $colonne = array("age",19, 20, 21),
+        $colonne = array("Lieu","Ambanja", "Diego", "Majunga")
     );
 
-    for ($ligne = 0; $ligne < 4; $ligne++) {
-        echo "<dl>";
-        for ($col = 0; $col < 2; $col++) {
-          echo "<dd>".$cars[$ligne][$col]."</dd>";
-        }
-        echo "</dl>";
+    echo $Personne[0][0].": ".$Personne[0][2].".<br>";
+    echo $Personne[1][0]." :".$Personne[1][2].".<br>";
+    echo $Personne[2][0].":".$Personne[2][2].".<br>";
+    echo $Personne[3][0].": ".$Personne[3][2].".<br>";
+    *****************************************************
+    afficher miora seulement
+    for ($ligne= 0; $ligne < count($colonne); $ligne++) {
+      echo "<dl>";
+      for ($col = 0; $col < count($colonne)-1; $col+=2) {
+        echo "<dd>".$Personne[$ligne][$col]."</dd>";
       }
+      echo "</dl>";
+    }
+
+    // echo $Personne[0][0].": ".$Personne[0][2].".<br>";
+    // echo $Personne[1][0]." :".$Personne[1][2].".<br>";
+    // echo $Personne[2][0].":".$Personne[2][2].".<br>";
+    // echo $Personne[3][0].": ".$Personne[3][2].".<br>";
+    // *****************************************************
+    // afficher miora seulement
+    // for ($ligne= 0; $ligne < count($colonne); $ligne++) {
+    //   echo "<dl>";
+    //   for ($col = 0; $col < count($colonne)-1; $col+=2) {
+    //     echo "<dd>".$Personne[$ligne][$col]."</dd>";
+    //   }
+    //   echo "</dl>";
+    // }
+    // *****************************************************
+    for ($ligne= 0; $ligne < count($Personne); $ligne++) {
+      echo "<dl>";
+      for ($col = 0; $col < count($colonne)-2; $col++) {
+        echo "<dd>".$Personne[$ligne][$col]."</dd>";
+      }
+      echo "</dl>";
+    }
 ?>
