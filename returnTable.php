9a25be5b40de37878b01404f9953b198eba6698c
naziradidier <nazirap20.aps1b@gmai.com>
2021-02-23 17:50:34 +0100

    tableau dimension pour nazirah, miora et fonction qui utilise include

---
+++ returnTable.php
@@ -0,0 +1,19 @@
+<?php
+    $Personne = array (
+        $colonne = array("Nom", "Nazirah", "Miora", "Toavina"),
+        $colonne = array("Prénom","Prisca", "Jelisca", "Mbola"),
+        $colonne = array("age",19, 20, 21),
+        $colonne = array("Lieu","Ambanja", "Diego", "Majunga")
+    );
+
+    function returnTable($table){
+        $table = array("Nazirah", "Miora", "Toavina");
+          echo "<ul>";
+          for ($i = 0; $i < count($table); $i++) {
+              echo "<li>";
+              echo "<a href=\"http://localhost/test/$table[$i].html.php\"> $table[$i]</a>";
+              echo "</li>";
+          }
+          echo "</ul>";
+          }  
+?>
