7d1dcfe5c3d00c5c5ca425737ea921f9d0095757
Nazirah <74833519+naziradidier@users.noreply.github.com>
2021-04-29 06:44:16 +0200

    Update tableauDimenionPersonne.php

--- tableauDimenionPersonne.php
+++ tableauDimenionPersonne.php
@@ -18,10 +18,10 @@ boucle pour afficher le tableau
     }
 
 // affiché nom: ...
-//     echo $Personne[0][0].": ".$Personne[0][2].".<br>";
-//     echo $Personne[1][0]." :".$Personne[1][2].".<br>";
-//     echo $Personne[2][0].":".$Personne[2][2].".<br>";
-//     echo $Personne[3][0].": ".$Personne[3][2].".<br>";
+    echo $Personne[0][0].": ".$Personne[0][2].".<br>";
+    echo $Personne[1][0]." :".$Personne[1][2].".<br>";
+    echo $Personne[2][0].":".$Personne[2][2].".<br>";
+    echo $Personne[3][0].": ".$Personne[3][2].".<br>";
     // *****************************************************
 //     afficher miora seulement
     for ($ligne= 0; $ligne < count($colonne); $ligne++) {
